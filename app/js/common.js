c7ac719ef6694cd04967efe9fee93963c69ac606
Sytnik Artem <artemasitnik@gmail.com>
2018-12-28 18:16:44 +0200

    1
    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,131 +1,52 @@
 $(document).ready(function() {
-    $('.main-partner-slider').slick({
-        infinite: true,
-        slidesToShow: 7,
-        slidesToScroll: 1,
+    if ($(window).width() > 1023) {
+        $('#fullpage').fullpage({
+            scrollBar:true,
+            controlArrows: false,
+            responsiveWidth: 1024,
+            onLeave: function(origin, destination, index){
+                if(destination.item.classList.contains('section_white')) {
+                    $('.menu, .header').addClass('active');
+                }
+                else {
+                    $('.menu, .header').removeClass('active');
+                }
+            }
+        });
+    }
+    $('.main-slider').slick({
+        infinite: false,
+        speed: 300,
         arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         responsive: [
             {
-                breakpoint: 992,
+                breakpoint: 1024,
                 settings: {
-                    arrows: false,
-                    slidesToShow: 5
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
                 }
             },
             {
-                breakpoint: 768,
+                breakpoint: 600,
                 settings: {
-                    // centerMode: true,
-                    variableWidth: true
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
                 }
             }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
         ]
-        // variableWidth: true
-    });
-    $('.certificate-slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows: false
-    });
-    var $body = $(window.document.body);
-
-    function bodyFreezeScroll() {
-        var bodyWidth = $body.innerWidth();
-        $body.css('overflow-y', 'hidden');
-        $body.css('marginRight', ($body.css('marginRight') ? '+=' : '') + ($body.innerWidth() - bodyWidth))
-    }
-
-    function bodyUnfreezeScroll() {
-        var bodyWidth = $body.innerWidth();
-        $body.css('marginRight', '0')
-        $body.css('overflow-y', 'auto');
-    }
-    $('.employees-item').click(function () {
-        $(this).find('.employees-popup').addClass('active');
-        var popupHeight = $(this).find('.employees-popup-block').outerHeight();
-        var windowHeight = $(window).height();
-        bodyFreezeScroll()
-        if(windowHeight < popupHeight + 100) {
-            $(this).find('.employees-popup').addClass('flex-start')
-        }
-    });
-    $('.employees-popup-close').click(function (e) {
-        e.stopPropagation();
-        bodyUnfreezeScroll();
-        $('.employees-popup, .news-popup').removeClass('active');
-        $('.employees-popup, .news-popup').removeClass('flex-start');
-    });
-    $('.news-item').click(function (e) {
-        e.preventDefault();
-        $('.news-popup').addClass('active');
-        bodyFreezeScroll();
-        var popupHeight = $('.news-popup-container').outerHeight();
-        var windowHeight = $(window).height();
-        console.log(popupHeight);
-        console.log(windowHeight);
-        bodyFreezeScroll();
-        if(windowHeight < popupHeight + 100) {
-            $('.news-popup').addClass('flex-start')
-        }
-    });
-    function fixHeight() {
-        var i;
-        var j;
-        var arr1 = [];
-        var arr2 = [];
-        for (i = 0; i < $('.news-item-block').length; i++) {
-            var blockHeight = $('.news-item-block').eq(i).height();
-            arr2.push(blockHeight);
-        }
-        for (j = 0; j < $('.employees-item-block').length; j++) {
-            var blockHeight2 = $('.employees-item-block').eq(j).height();
-            arr1.push(blockHeight2);
-        }
-        $('.news-item-block').height(Math.max(...arr2));
-        $('.employees-item-block').height(Math.min(...arr1));
-    }
-    if($(window).width() > 768) {
-        setTimeout(function(){
-            fixHeight();
-        },100)
-    }
-
-
-    $('.partners-right-link').click(function(e){
-        e.preventDefault();
-        $('.partners-info').removeClass('active');
-        $(this).parent().find('.partners-info').addClass('active');
-    });
-    $('.partners-info-close').click(function () {
-        $(this).parent().removeClass('active');
-    });
-    $('.certificate-item').click(function(){
-        $('.news-popup').addClass('active');
-        var index = $(this).index();
-        $('.certificate-slider').slick('slickGoTo', index);
-        var popupHeight = $('.certificate-slider-relative').outerHeight();
-        var windowHeight = $(window).height();
-        bodyFreezeScroll();
-        if(windowHeight < popupHeight + 100) {
-            $('.news-popup').addClass('flex-start')
-        }
-        bodyFreezeScroll();
-    });
-    $('.certificate-btn_next').click(function () {
-        $('.certificate-slider').slick('slickNext');
-    });
-    $('.certificate-btn_back').click(function () {
-        $('.certificate-slider').slick('slickPrev');
-    });
-    $('.header-btn').click(function () {
-       $(this).toggleClass('active');
-       $('.header-menu').toggleClass('active');
-       if($(this).hasClass('active')) {
-           bodyFreezeScroll();
-       }
-        else {
-           bodyUnfreezeScroll();
-       }
     });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -51,7 +51,10 @@ gulp.task('libs-css', ['sass'], function(){
 });
 gulp.task('libs-js', function(){
   return gulp.src([
-    'app/libs/slick-carousel/slick/slick.min.js'
+      'app/libs/slick-carousel/slick/slick.min.js',
+      'app/libs/fullpage.js/dist/fullpage.min.js',
+      'app/libs/fullpage.js/dist/fullpage.extensions.min.js',
+      'app/libs/wow/dist/wow.min.js'
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
